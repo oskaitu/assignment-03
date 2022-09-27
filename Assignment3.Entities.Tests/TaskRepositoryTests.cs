1393c88ac7506622c6fd8c8d22650cf536bae0dc
Oscar <oska@itu.dk>
2022-09-27 14:50:29 +0200

    started

--- Assignment3.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment3.Entities.Tests/TaskRepositoryTests.cs
@@ -1,5 +1,21 @@
 namespace Assignment3.Entities.Tests;
 
-public class TaskRepositoryTests
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Assignment3.Core;
+
+public sealed class TaskRepository : IDisposable
 {
+    private readonly KanbanContext _context;
+    private readonly TaskRepository _repository;
+
+    public TaskRepositoryTests(){
+        
+        var connection = new SqliteConnection("Filename:memory:");
+        connection.Open();
+
+    }
+
 }
+
+
--- Assignment3.Entities.Tests/Usings.cs
+++ Assignment3.Entities.Tests/Usings.cs
@@ -1,2 +1,5 @@
 global using FluentAssertions;
 global using Xunit;
+
+global using static Assignment3.Core.Response;
+global using static Assignment3.Core.State;
